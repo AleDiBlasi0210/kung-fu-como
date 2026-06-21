e0619f8cf268e7a18393953ea8cff6ea033ba48a
Alessandro Di Blasi <alessandro.diblasi2004gmail.com>
2026-06-21 17:00:40 +0200

    Vercel config

--- next.config.js
+++ next.config.js
@@ -1,11 +1,5 @@
 /** @type {import('next').NextConfig} */
-const basePath = process.env.BASE_PATH || ''
-
 const nextConfig = {
-  output: 'export',
-  trailingSlash: true,
-  basePath,
-  assetPrefix: basePath || undefined,
   images: {
     unoptimized: true,
     remotePatterns: [
